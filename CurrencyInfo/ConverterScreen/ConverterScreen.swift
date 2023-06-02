46899ac43bb420566decb65346970479bcd84339
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-06-02 12:01:56 +0300

    added table for fromCurrency btn, connected with action

--- CurrencyInfo/ConverterScreen/ConverterScreen.swift
+++ CurrencyInfo/ConverterScreen/ConverterScreen.swift
@@ -32,6 +32,9 @@ class ConverterScreen: UIViewController {
         viewModel = ConverterScreenViewModel(latestRatesService)
         
         bindViewModel()
+        
+        currenciesListTableView.delegate = self
+        currenciesListTableView.dataSource = self
 
     }
 
@@ -54,7 +57,49 @@ class ConverterScreen: UIViewController {
         //            .disposed(by: disposeBag)
         
     }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        addTableViewToScreen()
+//    }
+    var currenciesListTableView: UITableView! = {
+        
+        let mainTableView = UITableView(frame: CGRectZero)
 
+        mainTableView.backgroundColor = UIColor.clear
+        mainTableView.translatesAutoresizingMaskIntoConstraints = false
+        mainTableView.tableFooterView = UIView()
+
+        //        mainTableView.contentInsetAdjustmentBehavior = .never
+//        mainTableView.showsVerticalScrollIndicator = false
+        mainTableView.separatorStyle = .none
+       
+        mainTableView.estimatedRowHeight = 20.0
+        mainTableView.rowHeight = UITableView.automaticDimension
+        
+        mainTableView.layer.cornerRadius = 10
+        mainTableView.clipsToBounds = true
+        
+        return mainTableView
+        
+    }()
+    
+    func addTableViewToScreen(){
+        
+        
+        view.addSubview(currenciesListTableView)
+        
+        NSLayoutConstraint.activate([
+            currenciesListTableView.topAnchor.constraint(equalTo: fromBtn.bottomAnchor),
+            currenciesListTableView.leftAnchor.constraint(equalTo: fromBtn.leftAnchor),
+//            currenciesListTableView.rightAnchor.constraint(equalTo: fromBtn.rightAnchor),
+            currenciesListTableView.heightAnchor.constraint(equalToConstant: 200),
+            currenciesListTableView.widthAnchor.constraint(equalToConstant: fromBtn.frame.width * 2),
+
+//            currenciesListTableView.he.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    
     // MARK: Outlets
     // for labels
     @IBOutlet weak var fromBtn: UIButton!
@@ -79,6 +124,8 @@ class ConverterScreen: UIViewController {
     }
     
     @IBAction func fromCurrencyAction(_ sender: Any) {
+        
+        addTableViewToScreen()
     }
     
     @IBAction func openDetailsAction(_ sender: Any) {
@@ -87,3 +134,23 @@ class ConverterScreen: UIViewController {
 }
 
 
+extension ConverterScreen: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "mido"
+        cell.contentView.backgroundColor = .systemGray4
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currenciesListTableView.removeFromSuperview()
+    }
+}
